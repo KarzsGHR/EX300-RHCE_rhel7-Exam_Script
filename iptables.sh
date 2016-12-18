eee80ddb13491212ab8c2cfc859f9fca908aa900
dckidd452 <dckidd452@gmail.com>
2016-12-18 14:15:56 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,3 +1,4 @@
+#/bin/bash
 systemctl disable firewalld
 systemctl stop firewalld
 systemctl enable iptables
@@ -14,10 +15,12 @@ $i -P OUTPUT ACCEPT
 
 
 $i -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-$i -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j DNAT --to 172.25.16.11:22
+$i -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j DNAT --to 172.25.16.11:22 ##port forward
 $i -A INPUT -p icmp -j ACCEPT
+$i -A INPUT -p tcp --dport 2049  -j ACCEPT ##NFS4 Allow
+$i -A INPUT -p tcp --dport 3260  -j ACCEPT ##ISCSI Allow
 $i -A INPUT -i lo -j ACCEPT
-$i -A INPUT -p tcp -s 192.168.0.0/24 --dport 22 -j DROP
+$i -A INPUT -p tcp -s 192.168.0.0/24 --dport 22 -j DROP ##Restrict SSH
 $i -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
 $i -A INPUT -j REJECT --reject-with icmp-host-prohibited
 $i -A FORWARD -j REJECT --reject-with icmp-host-prohibited
